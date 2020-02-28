8eb3709bd96b63b8438b4a3a7d975cc1b1728c4b
igor <embiggenerd@gmail.com>
2020-02-27 22:26:37 -0500

    login and register form untested and unstyled

--- src/components/ProfLoginForm.js
+++ src/components/ProfLoginForm.js
@@ -1,14 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+const axios = require('axios')
 
 const ProfLoginForm = () => {
+  const [formState, setFormState] = useState({ username: "", password: "" })
+  const [error, setError] = useState("")
+
+  const handleSubmit = async (event) => {
+    try {
+      event.preventDefault()
+      const { data } = await axios.post("https://better-professor-app-1.herokuapp.com/api/auth/login", formState)
+      // handle token
+      // data: {
+      //     message: `Welcome ${saved.firstName}`,
+      //     user: {
+      //         id: saved.id,
+      //         username: saved.username,
+      //         "First Name": saved.firstName,
+      //         "Last Name": saved.lastName,
+      //                           email: saved.email
+      //     },
+      //     token
+      // }
+      // handle redirect
+      console.log(data)
+    } catch (e) {
+      console.log(e)
+      setError(e.response.errMessage)
+      setTimeout(setError(""), 3500)
+    }
+  }
+
+  const handleChange = (event) => {
+    setFormState({ ...formState, [event.target.name]: event.target.value })
+  }
+
   return (
     <div>
-      <p>
-        Starter Component: This is where all the code for the Professor Login
-        Form will live
-      </p>
+      <form onSubmit={handleSubmit}>
+        <input onChange={handleChange} name="username" value={formState.username} placeholder="username" />
+        <input onChange={handleChange} name="password" value={formState.password} placeholder="password" />
+      </form>
+      <span className={error ? "err-message" : "err-message hidden"}>{error || null}</span>
     </div>
   );
 };
+
+
 
 export default ProfLoginForm;
--- src/components/ProfRegisterForm.js
+++ src/components/ProfRegisterForm.js
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+const axios = require('axios')
 
 const ProfRegisterForm = () => {
+
+  const [formState, setFormState] = useState({ username: "", password: "" })
+  const [error, setError] = useState("")
+
+
+  const handleSubmit = async (event) => {
+    try {
+      event.preventDefault()
+      const { data } = await axios.post("https://better-professor-app-1.herokuapp.com/api/auth/register", formState)
+      console.log(data)
+    //   data: {
+    //     message: `Welcome ${saved['First Name']}`,
+    //     user: {...saved},
+    //     token
+    // }
+      // handle redirect
+    } catch (e) {
+      console.log(e)
+      setError(e.response.errMessage)
+      setTimeout(setError(""), 3500)
+    }
+  }
+
+  const handleChange = (event) => {
+    setFormState({ ...formState, [event.target.name]: event.target.value })
+  }
+
   return (
     <div>
-      <p>
-        Starter Component: This is where all the code for the Professor Register
-        Form will live
-      </p>
+      <form onSubmit={handleSubmit}>
+        <input onChange={handleChange} name="username" value={formState.username} placeholder="username" />
+        <input onChange={handleChange} name="password" value={formState.password} placeholder="password" />
+      </form>
+      <span className={error ? "err-message" : "err-message hidden"}>{error || null}</span>
     </div>
   );
 };
