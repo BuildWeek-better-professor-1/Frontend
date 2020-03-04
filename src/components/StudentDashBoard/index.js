29de75018e57fce4db683ee4543938ec6a668c9e
igor <embiggenerd@gmail.com>
2020-03-03 21:36:59 -0500

    Student Dashboard Setup

---
+++ src/components/StudentDashBoard/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const StudentDashBoard = ({ component: Component, ...props }) => {
+  return (
+    <h1>Student Dash Board</h1>
+  );
+};
+
+export default StudentDashBoard 
