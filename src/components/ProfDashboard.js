380722db4767bc41dde4b9a971c68119c8d90f59
Giovani Garfias <46204471+survani@users.noreply.github.com>
2020-03-04 01:32:45 -0800

    Fixed Layout of Dashboard. Should make more sense now.

--- src/components/ProfDashboard.js
+++ src/components/ProfDashboard.js
@@ -16,31 +16,54 @@ const ProfDashboard = props => {
       <div className="dashboard-main-container" key={"hello"}>
         <h2 className="dashboard-title">Dashboard</h2>
         <div className="box-task-container">
-          <div className="box-task">
+          <div className="box-task-1">
             <p className="box-title">Welcome</p>
             <p className="task-welcome-name"> {props.professor}</p>
+            <button
+              className="box-button"
+              title="Display your name"
+              onClick={fetchSingleProfessor}
+            >
+              Show Name
+            </button>
           </div>
-          <div className="box-task-container">
+          <section className="box-task-container">
             <div className="box-task">
-              <p className="box-title">Tasks Open</p>
-              <img className="task-circle" alt="circle" src={Circle} />
-              <p className="task-count-circle">{0}</p>
+              <p className="box-title">Reminders</p>
+              <div className="reminder-list-container">
+                {/* ========== DUMMY DATA ========== */}
+                <p className="reminder">Grade Giovani's Paper</p>
+                <p className="reminder">Get New Books For Students</p>
+                <p className="reminder">New Class Starting Today</p>
+                <p className="reminder">Get More Markers</p>
+                <p className="reminder">Talk with Giovani</p>
+                <p className="reminder">Set Up Parents Confrence</p>
+                <p className="reminder">Create a Website for Students</p>
+                <p className="reminder">Create a Website for Students</p>
+                {/* ========== [END] DUMMY DATA ========== */}
+              </div>
             </div>
-          </div>
+          </section>
           <div className="box-task-container">
-            <div className="box-task">
+            <div className="box-task-1">
               <p className="box-title">Dashboard Tools</p>
 
               {/* This "Add Professor" button is for testing that the data is flowing from state...
                it will work differently once server works again.*/}
 
-              <button className="box-button" onClick={fetchSingleProfessor}>
-                Add Professor
+              <button className="box-button">Add</button>
+              <button
+                className="box-button"
+                title="You must click reminder first"
+              >
+                Edit
               </button>
-              <button className="box-button">Edit Reminder</button>
-              {/* <button className="box-button">Add Student</button>
-              <button className="box-button">Edit Student</button>
-              <button className="box-button">Delete Student</button> */}
+              <button
+                className="box-button"
+                title="You must click reminder first"
+              >
+                Delete
+              </button>
             </div>
           </div>
         </div>
