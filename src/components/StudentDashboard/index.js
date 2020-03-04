bf7b95f3f51539ebdf21853992824b15333c5692
igor <embiggenerd@gmail.com>
2020-03-04 00:07:46 -0500

    custom hook performing polling and displaying results, needs testing

--- src/components/StudentDashboard/index.js
+++ src/components/StudentDashboard/index.js
@@ -1,36 +1,47 @@
 import React, { useEffect, useState } from "react";
 import { axiosWithAuth } from '../axiosWithAuth'
 import { decode } from 'jsonwebtoken'
-import axios from 'axios'
+import { useInterval } from '../../hooks'
 
 const StudentDashBoard = () => {
   const [id, setId] = useState(null)
+
+  const [reminders, setReminders] = useState([])
 
   useEffect(() => {
     const token = localStorage.getItem('token')
     const { subject } = decode(token)
     setId(subject)
-
-
   }, [])
 
   useEffect(() => {
-    const getReminders = async () => {
-      try {
-        const { data } = await axiosWithAuth().get(`api/students/${id}/reminders`)
-        console.log(data)
-        
-
-      } catch (error) {
-        console.log(error)
+    for (let i = 0; i < reminders.length; i++) {
+      if (reminders[i]["Due date"] <= Date.now()) {
+        reminders[i].passed_due = true
       }
     }
+  }, [reminders])
 
-    getReminders()
-  }, [id])
+  const getReminders = async () => {
+    try {
+      const { data } = await axiosWithAuth().get(`api/students/${id}/reminders`)
+      setReminders(data.data.reminders)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  useInterval(getReminders, 5000)
 
   return (
-    <h1>Student Dash Board</h1>
+    <>
+      <h1>Student Dash Board</h1>
+      {reminders.map(r =>
+        <div className={r.passed_due ? "reminder urgent" : "reminder"}>
+          <h3>{r["Project Name"]}</h3>
+          <p>{r["Descrition"]}</p>
+        </div>)}
+    </>
   );
 };
 
