092624258184797868b922f26c2a4d8e27506568
igor <embiggenerd@gmail.com>
2020-03-03 22:36:30 -0500

    adding studnet dashboard things

--- src/components/StudentDashBoard/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const StudentDashBoard = ({ component: Component, ...props }) => {
-  return (
-    <h1>Student Dash Board</h1>
-  );
-};
-
-export default StudentDashBoard 
---
+++ src/components/StudentDashboard/index.js
@@ -0,0 +1,20 @@
+import React, { useEffect } from "react";
+// import axiosWithAuth from '../axiosWithAuth'
+
+const StudentDashBoard = () => {
+  // useEffect(()=>{
+  //   const getReminders = async()=>{
+  //     const reminders = await axiosWithAuth()
+  //     console.log(reminders.data)
+  //   }
+  //   getReminders()
+  // }, [])
+
+
+
+  return (
+    <h1>Student Dash Board</h1>
+  );
+};
+
+export default StudentDashBoard 
