5971a838a509889f45e436354f543f0446af85ed
craig-paiva <craiglpaiva@gmail.com>
2020-03-04 23:29:09 -0500

    ProfRegisterForm CSS

--- src/components/StudentRegisterForm.js
+++ src/components/StudentRegisterForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import './styles/StudentRegisterForm.css'
 
 const StudentRegisterForm = () => {
   const [user, setUser] = useState({ username: "", password: "", firstName: "", lastName: "", email: "" })
