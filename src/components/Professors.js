7e3470adc7c7505466e4206e2626d36b2e16c36b
craig-paiva <craiglpaiva@gmail.com>
2020-03-05 22:57:07 -0500

    Add get rsponse

--- src/components/Professors.js
+++ src/components/Professors.js
@@ -1,3 +1,31 @@
 // Craig's component
 // make a get request using axios to 'https://better-professor-app-1.herokuapp.com//api/users/professor
-// display the array of professors 
+// display the array of professors
+
+import React from "react";
+import axios from "axios";
+
+
+export default class Professors extends React.Component {
+    state = {
+        profs: [],
+    };
+
+    componentDidMount() {
+        axios.get(`https://better-professor-app-1.herokuapp.com//api/users/professor`)
+            .then(res => {
+                this.setState({ profs: res.data });
+            });
+    }
+
+    render() {
+        return (
+            <ul>
+                {this.state.profs.map(professor => (
+                    <li> key={professor.id}</li>
+                ))}
+            </ul>
+        )
+    }
+}
+
